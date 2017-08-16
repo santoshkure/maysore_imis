0b2ca63772398235984bddc6ae6ac46b278123d1
prabhu <prabhu@bd42aff7-f8be-4bd5-bb75-8aff9c5d2159>
2017-08-16 11:36:36 +0000

    Code is comitted by saurabh for comman validation

--- framework/images/webapp/images/commonjs/commonValidation.js
+++ framework/images/webapp/images/commonjs/commonValidation.js
@@ -18,3 +18,53 @@
 			      else 
 			    	return true;
 			}
+		  /**Added by Saurabh Gupta on 11 aug 2017 for Alphabhet Non Mandatory feilds*/
+		    
+		    function alphabhetValidation1(element,msg) {   
+		          
+		        if(element=="")
+		         {
+		            return true;
+		         }
+		            
+		        else
+		        {
+		        
+		        if(element!="") {
+		       
+		         var regex=/^[a-zA-Z\s\.]{1,100}$/;
+		         var input = regex.test(element);
+		      
+		          if(input==false){
+		            var alphaExp = /(\w+)\s(\w+)/;
+		               
+		            if(element.match(alphaExp)){
+		              return true;
+		             }
+		                
+		            else
+		            {
+		              alert(msg);
+		              return false;
+		            }
+		          }
+		          else if(element.length==0)
+		             {
+		               alert(msg);
+		             }
+		            else if(input==true && element.length>0)
+		             {
+		               return true;
+		             }
+		            else{
+		                alert("Please enter minimum one character");
+		                  return false;
+		            }
+		         }    
+		       }  
+		     }
+		    function trimFunction(field)
+		    {
+		    var str = field.value;
+		    field.value = str.trim();
+		 }
