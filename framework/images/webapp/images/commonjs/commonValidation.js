c97771bc318188259a22e7c6619fdcee8ffd7de1
siddhi.rajput <siddhi.rajput@bd42aff7-f8be-4bd5-bb75-8aff9c5d2159>
2017-08-22 10:55:34 +0000

    added by Anubha

--- framework/images/webapp/images/commonjs/commonValidation.js
+++ framework/images/webapp/images/commonjs/commonValidation.js
@@ -67,4 +67,18 @@
 		    {
 		    var str = field.value;
 		    field.value = str.trim();
-		 }
+		    }
+		    
+		    function commonPS(divId)
+	         {
+	               var html="<html>";
+	                    html+= document.getElementById(divId).innerHTML;
+	                html+="</html>";
+	                
+	                var printWin = window.open('','','left=0,top=0,width=10,height=5,toolbar=0,scrollbars=0,status=0');
+	                printWin.document.write(html);
+	                printWin.document.close();
+	                printWin.focus();
+	                printWin.print();
+	                printWin.close();
+	         } 
