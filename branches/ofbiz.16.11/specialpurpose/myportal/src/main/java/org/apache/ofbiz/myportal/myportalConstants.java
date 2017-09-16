fc53d8f4bc0dac632f68107b8582b4fe2c150e17
siddhi.rajput <siddhi.rajput@bd42aff7-f8be-4bd5-bb75-8aff9c5d2159>
2017-09-16 05:02:42 +0000

    added by siddhi for add new java file

---
+++ branches/ofbiz.16.11/specialpurpose/myportal/src/main/java/org/apache/ofbiz/myportal/myportalConstants.java
@@ -0,0 +1,13 @@
+package org.apache.ofbiz.myportal;
+
+public interface myportalConstants {
+	
+	public static final String SUCCESS_MESSAGE="successMessage";
+    public static final String ERROR_MESSAGE = "error message";
+	
+	public static final int DEFAULT_PAGE=0;  
+	public static final int DEFAULT_LIST_SIZE = 10;
+	public static final String CANT_CREATE = "error message";
+	
+	
+}
