4c9cfb7228e88c5a7dd904d3c1941249427e44ff
prabhu <prabhu@bd42aff7-f8be-4bd5-bb75-8aff9c5d2159>
2017-10-06 06:25:38 +0000

    Commited by prabhu for validation on 06102017

--- branches/ofbiz.16.11/framework/images/webapp/images/commonjs/commonValidation.js
+++ branches/ofbiz.16.11/framework/images/webapp/images/commonjs/commonValidation.js
@@ -100,3 +100,168 @@
 		       return false;  
 		       }  
 		    }  
+		    
+		    /**validate Numeric only */
+			 function enterNumeric(value,field) 
+			{
+				 if (value == null || !value.toString().match(/^[-]?\d*\.?\d*$/)) 
+					 {
+					  alert(field);
+			            setTimeout(function(){field.focus()}, 10);
+					  
+					  return false;
+					 }	 
+				 return true;
+			}
+			 
+			 /** Validate Longitude*/
+			 function validateLongitudeforMP1(element,msg2)
+		     {
+		    	 var regex=/^[-]?([0-9]{1,3})?(\.[0-9]{1,10})?:([0-9]{1,2})?:([0-9]{1,2})?$/;
+
+		    	 var subvalue = element.split(":");
+
+		    	 var deg = subvalue[0];
+		    	 var min = subvalue[1];
+		    	 var sec = subvalue[2];
+
+		    	 var input = regex.test(element); 
+
+		    	 if(input==true)
+		    	 {
+		    		 if( deg < 0) 
+		    		 { 
+		    			 latsign = -1; 
+		    		 }
+
+		    		 absdlat = Math.abs( Math.round(deg * 1000000.));
+
+		    		 if(((absdlat > (73 * 1000000)) && (absdlat < (84 * 1000000))))
+		    		 { 
+
+		    			 min = Math.abs(Math.round(min * 1000000.)/1000000);  //integer
+		    			 absmlat = Math.abs(Math.round(min * 1000000.));  //integer
+
+		    			 //Error checks
+		    			 if(absmlat >= (60 * 1000000))
+		    			 { 
+		    				 alert(' Minutes Longitude must be in the range of 0 to 59. ');
+		    				 return false
+		    			 }
+		    			 else
+		    			 {
+		    				 sec = Math.abs(Math.round(sec * 1000000.)/1000000);
+		    				 absslat = Math.abs(Math.round(sec * 1000000.)); 
+		    				 //Error checks
+		    				 if(absslat > (59.99999999 * 1000000))
+		    				 {
+		    					 alert('Seconds Longitude must be in the range of 0 to 59. ');
+		    					 return false 
+		    				 } 
+		    				 else
+		    				 {
+
+		    					 return true;
+		    				 }
+
+		    			 }
+		    		 }
+		    		 else
+		    		 {
+		    			 alert(' Degrees Longitude must be in the range of 74 to 83. ');
+		    			 return false;
+
+		    		 }
+
+		    	 }
+		    	 
+		    	 if(element.length>0)
+		    	 {
+		    		 if(input == false)
+		    		 {
+		    		 alert(msg2);
+		    		 return false;
+		    		 }
+		    	 }
+		    	 else
+		    	 {
+		    		 return true;
+		    	 }
+		     }
+
+			 
+			 /** Validate Latitude*/
+			 function validateLatitudeforMP1(element,msg2)
+		     {
+		   	  		 	 var regex=/^[-]?([0-9]{1,2})?(\.[0-9]{1,10})?:([0-9]{1,2})?:([0-9]{1,2})?$/;
+		   	  			 
+		   	  			 var subvalue = element.split(":");
+		   	  			 
+		   	  			 var deg = subvalue[0];
+		   	  			 var min = subvalue[1];
+		   	  			 var sec = subvalue[2];
+		   	  			 
+		   	  			 var input = regex.test(element); 
+		   	  	 		 
+		   	  	       if(input==true)
+		   	  				 {
+		   	  				   
+		   	  					if( deg < 0) 
+		   	  						{ 
+		   	  						latsign = -1; 
+		   	  						}
+		   	  					
+		   	  						absdlat = Math.abs( Math.round(deg * 1000000.));
+		   	  					// As per MP if(((absdlat > (20 * 1000000)) && (absdlat < (28 * 1000000))))	
+		   	  						
+		   	  					if(((absdlat > (7 * 1000000)) && (absdlat < (15 * 1000000))))
+		   	  						{ 
+		                    		min = Math.abs(Math.round(min * 1000000.)/1000000);  //integer
+		  	  						    absmlat = Math.abs(Math.round(min * 1000000.));  //integer
+
+		  	  						     //Error checks
+		  	  						     if(absmlat >= (60 * 1000000))
+		  	  						     { 
+		  	  						     alert(' Minutes Latitude must be in the range of 0 to 59. ');
+		  	  						     return false
+		  	  						     }
+		  	  						     else
+		  	  						     {
+		  	  						    	sec = Math.abs(Math.round(sec * 1000000.)/1000000);
+		  	  							     absslat = Math.abs(Math.round(sec * 1000000.));         // Note: kept as big integer for now, even if submitted as decimal
+
+		  	  							     //Error checks
+		  	  							     if(absslat > (59.99999999 * 1000000))
+		  	  							     {
+		  	  							      alert(' Seconds Latitude must be in the range of 0 to 59.');
+		  	  							     return false 
+		  	  							     } 
+		  	  							     else
+		  	  							     {
+		  	  							    	 
+		  	  							    	 return true;
+		  	  							     }
+		  	  						    	 
+		  	  						     }
+		   	  						
+		   	  						}
+		   	  						else
+		   	  						{
+		   	  						alert(' Degrees Latitude must be in the range of 8 to 14. ');
+			  							return false;
+		   	  						}
+		   	  					   
+		   	  				 } 
+		   	  	  if(element.length>0)
+		     	 {
+		     		 if(input == false)
+		     		 {
+		     		 alert(msg2);
+		     		 return false;
+		     		 }
+		     	 }
+		     	 else
+		     	 {
+		     		 return true;
+		     	 }
+		     }
